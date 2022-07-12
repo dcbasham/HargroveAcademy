ad2f241300f25097284401f02852f81278682df1
daurie basham <dauriebasham@gmail.com>
2022-07-12 13:42:58 -0500

    seed file and requirements file updated, pacakges json gitignore

--- seed.js
+++ seed.js
@@ -1,9 +1,45 @@
-const { green, red } = require("chalk");
-const { db } = require("./server/db");
+const { green, red } = require('chalk');
+const { db, Campus, Student } = require('./server/db');
 
 const seed = async () => {
   try {
     await db.sync({ force: true });
+    const Allbright = await Campus.create({
+      name: 'Allbright',
+      address: '124 Cornwall Ave.',
+      description:
+        "I'm baby ethical wayfarers vinyl poutine air plant neutra shoreditch direct trade deep v iPhone offal",
+    });
+    const Marfabulb = await Campus.create({
+      name: 'Marfabulb',
+      address: '345 Sesame Street',
+      description:
+        'Cliche scenester vice chillwave keytar, waistcoat locavore whatever',
+    });
+    const TaxidermyArt = await Campus.create({
+      name: 'TaxidermyArt',
+      address: '987 Hexagon St.',
+      description:
+        'Unicorn hell of swag street art taxidermy gluten-free. Raw denim irony selfies bitters aesthetic vinyl, hot chicken hashtag ',
+    });
+    const Jane = await Student.create({
+      firstName: 'Jane',
+      lastName: 'Smith',
+      email: 'JaneSmith@yahoo.com',
+      campusId: Marfabulb.id,
+    });
+    const Will = await Student.create({
+      firstName: 'Will',
+      lastName: 'Jones',
+      email: 'BillyBoy300@gmail.com',
+      campusId: TaxidermyArt.id,
+    });
+    const George = await Student.create({
+      firstName: 'George',
+      lastName: 'McCallan',
+      email: 'brewster45@gmail.com',
+      campusId: Marfabulb.id,
+    });
 
     // seed your database here!
   } catch (err) {
@@ -18,11 +54,11 @@ module.exports = seed;
 if (require.main === module) {
   seed()
     .then(() => {
-      console.log(green("Seeding success!"));
+      console.log(green('Seeding success!'));
       db.close();
     })
-    .catch(err => {
-      console.error(red("Oh noes! Something went wrong!"));
+    .catch((err) => {
+      console.error(red('Oh noes! Something went wrong!'));
       console.error(err);
       db.close();
     });
