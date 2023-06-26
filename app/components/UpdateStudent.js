e536769e08eb3b66e740b9b784dd165674d9391f
dcbasham <dauriebasham@gmail.com>
2023-06-26 17:24:49 -0500

    update student form validation, react rendering, database all good

--- app/components/UpdateStudent.js
+++ app/components/UpdateStudent.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { customStyles } from '../_customStyle';
+import ReactDOM from 'react-dom';
+import '../../public/_custom.css';
 import { updateStudent } from '../redux/students';
 import { connect } from 'react-redux';
-import { Form, Button, FormControl } from 'react-bootstrap';
 import { fetchSingleStudent } from '../redux/singleStudent';
 
 class UpdateStudent extends React.Component {
@@ -14,18 +14,23 @@ class UpdateStudent extends React.Component {
       lastName: '',
       email: '',
     };
+    this.form = React.createRef();
+    this.validate = this.validate.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
-
-  handleSubmit(evt) {
-    evt.preventDefault();
+  validate() {
+    this.form.current.reportValidity();
+  }
+  handleSubmit() {
+    this.validate();
     this.props.updateStudent(this.props.id, { ...this.state });
     this.setState({
       firstName: '',
       lastName: '',
       email: '',
     });
+
     return this.state.student;
   }
   handleChange(evt) {
@@ -34,55 +39,52 @@ class UpdateStudent extends React.Component {
     });
   }
   render() {
-    const { labelStyle } = customStyles;
     const { firstName, lastName, email } = this.state;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="form-group">
-          <label style={labelStyle} htmlFor="firstName">
-            First Name:
-          </label>
+      <div>
+        <form id="update" ref={this.form} onSubmit={this.handleSubmit}>
+          <label htmlFor="firstName">First Name:</label>
           <input
             type="text"
             name="firstName"
-            aria-placeholder="Alphabetical characters only"
+            placeholder="Alphabetical characters only"
             value={firstName}
             onChange={this.handleChange}
-            pattern="[A-Za-z]+$"
-            title="Alphabetical characters"
+            pattern="[A-Za-z \.]+$"
+            title="Alphabetical and space characters"
           />
-        </div>
-        <div className="form-group">
-          <label style={labelStyle} htmlFor="lastName">
-            Last Name:
-          </label>
+          <label htmlFor="lastName">Last Name:</label>
           <input
             type="text"
             name="lastName"
             value={lastName}
             onChange={this.handleChange}
-            aria-placeholder="Alphabetical characters only"
-            pattern="[A-Za-z]+$"
-            title="Alphabetical characters"
+            placeholder="Alphabetical characters"
+            pattern="[A-Za-z \.]+$"
+            title="Alphabetical and space characters"
           />
-        </div>
-        <div className="form-group">
-          <label style={labelStyle} htmlFor="email">
-            email :
-          </label>
+          <label htmlFor="email">Email :</label>
           <input
+            required
             type="email"
             name="email"
+            id="email"
             value={email}
             onChange={this.handleChange}
-            aria-placeholder="Valid email address"
+            placeholder="name@domain.com"
             title='Valid email address containing "@" and ".com"'
+            pattern="\s*[a-zA-Z0-9._\-]+\s*|\s*[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,4}\s*"
           />
-        </div>
-        <Button style={labelStyle} variant="outline-success" type="submit">
-          Update
-        </Button>
-      </form>
+
+          <button
+            type="submit"
+            className="btn-outline-success"
+            onClick={this.validate}
+          >
+            Update
+          </button>
+        </form>
+      </div>
     );
   }
 }
